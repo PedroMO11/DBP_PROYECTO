4567cd6f067c92a20ef1e02594302d84d9f8d31f
Pedro Mori <pedro.mori@utec.edu.pe>
2021-06-09 19:13:11 -0500

    Arreglo de errores de relationship e insercion de datos

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ db = SQLAlchemy(app)
 class Usuario(db.Model):
     __tablename__ = 'usuario'
     user = db.Column(db.String(80), primary_key = True)
-    contraseña = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     es_admin = db.Column(db.Boolean, nullable=False)
-    personal = db.relationship('PersonalMedico', backref = 'usuario', uselist = False)
-    paciente = db.relationship('Usuario', backref = 'usuario', uselist = False)
+    #personal = db.relationship('PersonalMedico', backref = 'usuario', uselist = False)
+    #paciente = db.relationship('Usuario', backref = 'usuario', uselist = False)
 
     def __repr__(self):
-        return f'<Usuario: {self.user}, {self.contraseña}, {self.es_admin}>'
+        return f'<Usuario: {self.user}, {self.password}, {self.es_admin}>'
 
-    def __init__(self, user, contraseña, Es_Admin):
+    def __init__(self, user, password, es_admin):
         self.user = user
-        self.contraseña = contraseña
-        self.Es_Admin = Es_Admin
+        self.password = password
+        self.es_admin = es_admin
 
 class PersonalMedico(db.Model):
     __tablename__ = 'personal'
@@ -47,8 +47,8 @@ class Residencia(db.Model):
     direccion = db.Column(db.String(80), nullable = False)
     no_habitaciones = db.Column(db.Integer)
     director = db.Column(db.String(20), nullable = False)
-    personal = db.relationship('PersonalMedico', backref = 'residencia')
-    pacientes = db.relationship('Paciente', backref = 'residencia')
+    #personal = db.relationship('PersonalMedico', backref = 'residencia')
+    #pacientes = db.relationship('Paciente', backref = 'residencia')
 
     def __repr__(self):
         return f'<Residencia: {self.id}, {self.nombre}, {self.direccion}, {self.no_habitaciones}, {self.director}>'
@@ -67,7 +67,7 @@ class Paciente(db.Model):
     apellido = db.Column(db.String(80), nullable = False)
     titulo = db.Column(db.String(80), nullable = False)
     especialidad = db.Column(db.String(80), nullable = False)
-    residencia = db.Column(db.String(20), db.ForeignKey('residencia.id'), nullable = False)
+    residencia = db.Column(db.String(20), db.ForeignKey('residencia.nombre'), nullable = False)
     usuario = db.Column(db.String(80), db.ForeignKey('usuario.user'), nullable = False)
     def __repr__(self):
         return f'<Paciente : {self.dni}, {self.nombre}, {self.apellido}, {self.especialidad}, {self.residencia}, {self.usuario}>'
@@ -83,36 +83,43 @@ class Paciente(db.Model):
 
 db.create_all()
 
-'''
 sanjuan = Residencia("San Juan", "Surco", 21, "Carmen Durant")
 losportales = Residencia("Los Portales", "La Molina", 23, "Erika Guillen")
 
-usuario1 = Usuario("sgutierrez", "1234")
-usuario2 = Usuario("fsantos", "1234")
-usuario3 = Usuario("egambirazio", "1234")
-usuario4 = Usuario("rvega", "1234")
-usuario5 = Usuario("lrodriguez", "1234")
-usuario6 = Usuario("fnovoa", "1234")
-usuario7 = Usuario("mchavez", "1234")
-usuario8 = Usuario("lbalta", "1234")
-usuario9 = Usuario("hdurand", "1234")
-usuario10 = Usuario("dtafur", "1234")
+usuario1 = Usuario("sgutierrez", "1234", False)
+usuario2 = Usuario("fsantos", "1234", False)
+usuario3 = Usuario("egambirazio", "1234", False)
+usuario4 = Usuario("rvega", "1234", False)
+usuario5 = Usuario("lrodriguez", "1234", False)
+usuario6 = Usuario("fnovoa", "1234", False)
+usuario7 = Usuario("mchavez", "1234", False)
+usuario8 = Usuario("lbalta", "1234", False)
+usuario9 = Usuario("hdurand", "1234", False)
+usuario10 = Usuario("dtafur", "1234", False)
 
-medico1 = PersonalMedico(71284499, "Santiago", "Gutierrez", "Doctor", "Neurólogía", usuario1)
-medico2 = PersonalMedico(84909223, "Fabricio", "Santos", "Doctor", "Cardiólogía", usuario2)
-medico3 = PersonalMedico(84909223, "Esteban", "Gambirazio", "Doctor", "Traumatólogía", usuario3)
-medico4 = PersonalMedico(72876544, "Romario", "Vega", "Doctor", "Neurólogía", usuario4)
-medico5 = PersonalMedico(62345678, "Luis", "Rodriguez", "Doctor", "Endocrinología", usuario5)
-enfermera1 = PersonalMedico(10278990, "Fátima", "Novoa", "Enfermera", "Geriatría", usuario6)
-enfermera2 = PersonalMedico(10278990, "Micaela", "Chavez", "Enfermera", "Geriatría", usuario7)
-enfermera3 = PersonalMedico(10278990, "Luciana", "Balta", "Enfermera", "Geriatría", usuario8)
-enfermera4 = PersonalMedico(10278990, "Haydi", "Durand", "Enfermera", "Geriatría", usuario9)
-enfermera5 = PersonalMedico(10278990, "Daniela", "Tafur", "Enfermera", "Geriatría", usuario10)
-'''
+db.session.add_all( [sanjuan, losportales, usuario1, usuario2, usuario3, usuario4, usuario5, usuario6, 
+usuario7, usuario8, usuario9, usuario10])
+db.session.commit()
+
+medico1 = PersonalMedico(71284499, "Santiago", "Gutierrez", "Doctor", "Neurólogía", "sgutierrez")
+medico2 = PersonalMedico(84909223, "Fabricio", "Santos", "Doctor", "Cardiólogía", "fsantos")
+medico3 = PersonalMedico(83919223, "Esteban", "Gambirazio", "Doctor", "Traumatólogía", "egambirazio")
+medico4 = PersonalMedico(72876544, "Romario", "Vega", "Doctor", "Neurólogía", "rvega")
+medico5 = PersonalMedico(62345678, "Luis", "Rodriguez", "Doctor", "Endocrinología", "lrodriguez")
+enfermera1 = PersonalMedico(10278990, "Fátima", "Novoa", "Enfermera", "Geriatría", "fnovoa")
+enfermera2 = PersonalMedico(10278991, "Micaela", "Chavez", "Enfermera", "Geriatría", "mchavez")
+enfermera3 = PersonalMedico(10278992, "Luciana", "Balta", "Enfermera", "Geriatría", "lbalta")
+enfermera4 = PersonalMedico(10278993, "Haydi", "Durand", "Enfermera", "Geriatría", "hdurand")
+enfermera5 = PersonalMedico(10278994, "Daniela", "Tafur", "Enfermera", "Geriatría", "dtafur")
+
+db.session.add_all( [medico1, medico2, medico3, medico4, medico5, enfermera1, 
+enfermera2, enfermera3, enfermera4, enfermera5]
+)
+db.session.commit()
 
 @app.route('/')
 def index():
-    return "Hola"
+    return "<h1> April la más fea </h1>"
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5002, debug = True)
