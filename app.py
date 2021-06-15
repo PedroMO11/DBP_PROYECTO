57f6f19955673aa304b02db56f777d6f083b66b6
Pedro Mori <pedro.mori@utec.edu.pe>
2021-06-15 16:31:21 -0500

    Se agregó la ruta de registro en el front-end y la lógica para el registro de un paciente en la BD

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/proyecto'    #Editar para cada integrante la db respectiva con usuario y contrasenia
@@ -11,13 +12,9 @@ migrate = Migrate(app, db)
 class Usuario(db.Model):
     __tablename__ = 'usuario'
     user = db.Column(db.String(80), primary_key = True)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(8), nullable=False)
     es_admin = db.Column(db.Boolean, nullable=False)
     personal = db.relationship('PersonalMedico', backref = 'usuario_personal', uselist = False, primaryjoin= "Usuario.user == foreign(PersonalMedico.usuario)")
-    paciente = db.relationship('Paciente', backref = 'usuario_paciente', uselist = False, primaryjoin= "Usuario.user == foreign(Paciente.usuario)")
-
-    def __repr__(self):
-        return f'<Usuario: {self.user}, {self.password}, {self.es_admin}>'
 
     def __init__(self, user, password, es_admin):
         self.user = user
@@ -33,9 +30,6 @@ class PersonalMedico(db.Model):
     especialidad = db.Column(db.String(80), nullable = False)
     usuario = db.Column(db.String(80), db.ForeignKey('usuario.user'), nullable = False)
     residencia = db.Column(db.String(80), db.ForeignKey('residencia.nombre'), nullable = False)
-    
-    def __repr__(self):
-        return f'<Usuario: {self.dni}, {self.nombre}, {self.apellido}, {self.titulo}, {self.especialidad}, {self.usuario}>'
     
     def __init__(self, dni, nombre, apellido, titulo, especialidad, usuario, residencia):
         self.dni = dni
@@ -55,9 +49,6 @@ class Residencia(db.Model):
     personal = db.relationship('PersonalMedico', backref = 'residencia_personal', lazy = "dynamic", primaryjoin = "Residencia.nombre == PersonalMedico.residencia")
     pacientes = db.relationship('Paciente', backref = 'residencia_paciente', lazy = "dynamic",  primaryjoin= "Residencia.nombre == Paciente.residencia")
 
-    def __repr__(self):
-        return f'<Residencia: {self.id}, {self.nombre}, {self.direccion}, {self.no_habitaciones}, {self.director}>'
-
     def __init__(self, nombre, direccion, no_habitaciones, director):
         self.nombre = nombre
         self.direccion = direccion
@@ -70,26 +61,50 @@ class Paciente(db.Model):
     dni = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(80), nullable = False)
     apellido = db.Column(db.String(80), nullable = False)
-    titulo = db.Column(db.String(80), nullable = False)
-    especialidad = db.Column(db.String(80), nullable = False)
+    edad = db.Column(db.Integer, nullable = False)
+    habitacion = db.Column(db.Integer, nullable = False)
     residencia = db.Column(db.String(20), db.ForeignKey('residencia.nombre'), nullable = False)
-    usuario = db.Column(db.String(80), db.ForeignKey('usuario.user'), nullable = False)
 
-    def __repr__(self):
-        return f'<Paciente : {self.dni}, {self.nombre}, {self.apellido}, {self.especialidad}, {self.residencia}, {self.usuario}>'
-
-    def __init__(self, dni, nombre, apellido, titulo, especialidad, residencia, usuario):
+    def __init__(self, dni, nombre, apellido, edad, habitacion, residencia):
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
-        self.titulo = titulo
-        self.especialidad = especialidad
+        self.edad = edad
+        self.habitacion = habitacion
         self.residencia = residencia
-        self.usuario = usuario
+        
 
 @app.route('/')
 def index():
-    return "<h1> Hola </h1>"
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return "hola"
+
+@app.route('/registro', methods=['POST'])
+def registro_paciente():
+    #error = False
+    #response = {}
+    
+    dni = request.form.get('dni')
+    nombre = request.form.get('nombre')
+    apellido = request.form.get('apellido')
+    edad = request.form.get('edad')
+    habitacion = request.form.get('habitacion')
+    residencia = request.form.get('residencia')
+    paciente = Paciente(dni, nombre, apellido, edad, habitacion, residencia)
+    db.session.add(paciente)
+    db.session.commit()
+    '''
+    except:
+        #error = True
+        db.session.rollback()
+        print(sys.exc_info)
+    finally:
+        db.session.close()
+    '''
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5002, debug = True)
