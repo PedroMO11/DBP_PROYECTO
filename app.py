d194ff870b7ee42458783cd96698a25d748ec8f8
Pedro Mori <pedro.mori@utec.edu.pe>
2021-07-31 15:56:45 -0500

    API REST added

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from flask.helpers import flash
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://postgres:1234@localhost:5432/test2"
+] = "postgresql://postgres:123456@localhost:5432/proyecto"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "hjklhklhlk"
 db = SQLAlchemy(app)
@@ -106,9 +106,7 @@ class Paciente(db.Model):
     usuario = db.Column(db.String(80), db.ForeignKey("usuario.user"))
 
     def __repr__(self):
-        return
-        f"<Paciente : {self.dni}, {self.nombre}, "
-        "{self.apellido}, {self.edad}, f{self.residencia}, {self.usuario}>"
+        return f"<Paciente : {self.dni}, {self.nombre}, {self.apellido}, {self.edad}, {self.residencia}, {self.usuario}>"
 
 
 login_manager = LoginManager()
@@ -375,9 +373,219 @@ def delete_paciente_by_id(paciente_dni):
             usuario = Usuario.query.get(user.id)
             db.session.delete(usuario)
         db.session.commit()
+    return jsonify(response)
+
+#------------------------------------API REST-------------------------------------------------------------
+
+@app.route("/api/login", methods = ["POST"])
+def api_login():
+    usuario = request.json["user"]
+    contraseña = request.json["password"]
+    user = Usuario.query.filter_by(user=usuario).first()
+    if user:
+        if user.password == contraseña:
+            login_user(user, remember=True)
+            return jsonify(
+                message = "Correcto inicio de sesion")
+        else:
+            return jsonify(
+                message = "Contraseña incorrecta")
+    else:
+        return jsonify(
+                message = "Usuario no existe")
+
+@app.route("/api/usuarios/<user>/registrar", methods=["POST"])
+def api_registrar_usuario(user):
+    error = False
+    response = {}
+    try:
+        usuario = request.json["user"]
+        password = request.json["password"]
+        es_admin = request.json["es_admin"]
+        dni = request.json["dni"]
+        personal = PersonalMedico.query.get(dni)
+        persona = Paciente.query.get(dni)
+        if es_admin == "1":
+            admin = True
+        else:
+            admin = False
+        if len(dni) != 8:
+            response["message"] = "DNI inválido"
+        else:
+            if admin:
+                if persona is None and personal is None:
+                    user = Usuario.query.filter_by(user=usuario).first()
+                    if user:
+                        response["message"] = "Usuario ya existe"
+                    else:
+                        nombre = request.json["nombre"]
+                        apellido = request.json["apellido"]
+                        titulo = request.json["titulo"]
+                        especialidad = request.json["especialidad"]
+                        res = request.json["residencia_id"]
+                        residencia = Residencia.query.get(res)
+                        nuevoUsuario = Usuario(
+                            user=usuario, password=password, es_admin=admin
+                        )
+                        nuevoPersonal = PersonalMedico(
+                            dni=dni,
+                            nombre=nombre,
+                            apellido=apellido,
+                            titulo=titulo,
+                            especialidad=especialidad,
+                            residencia_personal=residencia,
+                            usuario_personal=nuevoUsuario,
+                        )
+                        db.session.add(nuevoUsuario)
+                        db.session.add(nuevoPersonal)
+                        db.session.commit()
+                        response["message"] = "Usuario creado correctamente"
+            else:
+                if persona:
+                    if persona.usuario is None:
+                        user = Usuario.query.filter_by(user=usuario).first()
+                        if user:
+                            response["message"] = "Usuario ya existe"
+                        else:
+                            nuevoUsuario = Usuario(
+                                user=usuario, password=password, es_admin=admin
+                            )
+                            persona.usuario_paciente = nuevoUsuario
+                            db.session.add(nuevoUsuario)
+                            db.session.commit()
+                            response["message"] = "Usuario creado"
+                    else:
+                        response["message"] = "Paciente ya registrado"
+                else:
+                    response["message"] = "DNI de paciente no existe"
+    except:
+        error = True
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+    if error:
+        response["message"] = "Error en el BE"
+
+    response["error"] = error
 
     return jsonify(response)
 
+@app.route("/api/registro_paciente", methods=["POST"])
+#@login_required
+def api_registro_paciente():
+    dni = request.json["dni"]
+    persona = Paciente.query.get(dni)
+    if persona:
+        return jsonify(message = "Persona con este DNI ya ha sido registrada")
+    else:
+        nombre = request.json["nombre"]
+        apellido = request.json["apellido"]
+        edad = request.json["edad"]
+        habitacion = request.json["habitacion"]
+        res = request.json["residencia"]
+        residencia = Residencia.query.get(res)
+        paciente = Paciente(
+            dni=dni,
+            nombre=nombre,
+            apellido=apellido,
+            edad=edad,
+            habitacion=habitacion,
+            residencia_paciente=residencia,
+        )
+        db.session.add(paciente)
+        db.session.commit()
+        return jsonify(message = "Paciente registrado correctamente")
 
+
+@app.route("/api/ver_pacientes", methods = ["GET"])
+#@login_required
+def api_ver_pacientes():
+    pacientes_list = []
+    for paciente in Paciente.query.all():
+        paciente_dict = {}
+        paciente_dict["dni"] = paciente.dni
+        paciente_dict["nombre"] = paciente.nombre
+        paciente_dict["apellido"] = paciente.apellido
+        paciente_dict["edad"] = paciente.edad
+        paciente_dict["habitacion"] = paciente.habitacion
+        paciente_dict["residencia"] = paciente.residencia_paciente.nombre
+        paciente_dict["usuario"] = paciente.usuario
+        pacientes_list.append(paciente_dict)
+    return jsonify(pacientes_list)
+
+@app.route("/api/pacientes/<paciente_id>/registrar", methods=["POST"])
+#@login_required
+def api_registrar_paciente(paciente_id):
+    error = False
+    response = {}
+    persona = Paciente.query.get(paciente_id)
+    try:
+        if len(paciente_id) != 8:
+            response["message"] = "DNI inválido"
+        elif persona:
+            response["message"] = "Persona con este DNI ya ha sido registrada"
+            response["category"] = "error"
+        else:
+            nombre = request.json["nombre"]
+            apellido = request.json["apellido"]
+            edad = request.json["edad"]
+            habitacion = request.json["habitacion"]
+            res = request.json["residencia_id"]
+            residencia = Residencia.query.get(res)
+            paciente = Paciente(
+                dni=paciente_id,
+                nombre=nombre,
+                apellido=apellido,
+                edad=edad,
+                habitacion=habitacion,
+                residencia_paciente=residencia,
+            )
+            db.session.add(paciente)
+            db.session.commit()
+            response["message"] = "Paciente registrado correctamente"
+            response["category"] = "success"
+    except:
+        error = True
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+    if error:
+        response["message"] = "Error en el BE"
+
+    response["error"] = error
+    return jsonify(response)
+
+@app.route("/api/pacientes/<paciente_dni>/editar", methods=["POST"])
+#@login_required
+def api_editar_paciente(paciente_dni):
+    error = False
+    response = {}
+    try:
+        dni = request.json["dni"]
+        persona = Paciente.query.get(dni)
+        res = request.json["residencia_id"]
+        residencia = Residencia.query.get(res)
+        persona.nombre = request.json["nombre"]
+        persona.apellido = request.json["apellido"]
+        persona.edad = request.json["edad"]
+        persona.habitacion = request.json["habitacion"]
+        persona.residencia_paciente = residencia
+        db.session.commit()
+        response["message"] = "Paciente editado correctamente"
+    except:
+        error = True
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+    if error:
+        response["message"] = "Error en BE"
+
+    response["error"] = error
+    return jsonify(response)
+
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
